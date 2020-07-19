de2d8a6e4842230b7f40c5616c81f718c010b521
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2020-07-19 19:10:32 -0400

    Init repo

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Yuri Ramocan on 7/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Yuri Ramocan on 7/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
