cf2d785f2c7abc888242aa55290b7d8092b9cee3
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2020-07-19 22:23:14 -0400

    Remove unused views

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -1,58 +1,5 @@
 import SwiftUI
 
-struct PrimaryView: View {
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Header")
-                .font(.title)
-                .bold()
-
-            Spacer()
-
-            VStack(alignment: .leading) {
-                HStack {
-                    Image(systemName: "checkmark.circle.fill")
-                        .foregroundColor(.green)
-                    Text("Step 1")
-                }
-
-                HStack {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundColor(.gray)
-                    Text("Step 2")
-                }
-
-                HStack {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundColor(.gray)
-                    Text("Step 3")
-                }
-
-                HStack {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundColor(.gray)
-                    Text("Step 4")
-                }
-            }
-
-            Spacer()
-
-            Button(action: {}) {
-                HStack {
-                    Image(systemName: "plus.circle")
-                    Text("Add Adjustment")
-                }
-            }
-        }
-    }
-}
-
-struct DetailView: View {
-    var body: some View {
-        Text("Detail View")
-    }
-}
-
 struct ContentView: View {
     @ObservedObject var favorites = Favorites()
 
